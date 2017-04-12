c6454cd1f42beb23510419549c9f5e095c513341
carlganz <carlganz@gmail.com>
2017-04-12 05:20:06 +0000

    try to fix tests

--- tests/testthat/test-login.R
+++ tests/testthat/test-login.R
@@ -1,9 +1,9 @@
 context("Test docuSign login")
 
 test_that("Login doesn't error", {
-  expect_success(login <- docu_login())
+  expect_silent(login <- docu_login())
 })
 
-# test_that("An actual account is returned", {
-#   expect_true(nrow(login)>0)
-# })
+test_that("An actual account is returned", {
+  expect_true(nrow(login)>0)
+})
