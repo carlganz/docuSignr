0b70abc2fac2c989fa9e17c209e9728a955ae4e4
carlganz <carlganz@gmail.com>
2017-04-12 05:53:38 +0000

    test env vars

--- tests/testthat/test-1-login.R
+++ tests/testthat/test-1-login.R
@@ -1,5 +1,11 @@
 context("Test docuSign login")
 
+test_that("Environmental vars exist", {
+  expect_true(Sys.getenv("docuSign_username")!="")
+  expect_true(Sys.getenv("docuSign_password")!="")
+  expect_true(Sys.getenv("docuSign_integrator_key")!="")
+})
+
 test_that("Login doesn't error", {
   expect_silent(login <- docu_login())
 })
